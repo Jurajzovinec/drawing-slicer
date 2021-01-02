9d30c1c1232d2102db833bed7a6634d8018a09fc
Juraj Zovinec <juraj.zovinecc@gmail.com>
2021-01-02 19:25:15 +0100

    Adding of gitignore file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const services = require("./lib/services");
 const cors = require('cors');
-const path = require('path');
-const serveStatic = require('serve-static');
 const app = express();
 const port = process.env.PORT || 5050;
 
@@ -12,27 +10,10 @@ if(process.env.NODE_ENV === 'production') {
     app.use(cors({ origin: "http://localhost:3000" }));
 }
 
-//app.use(serveStatic(__dirname));
-
 app.get('/slice', (req, res) => {
     services.sliceService();
-/*     const { spawn } = require('child_process');
-    const childPython = spawn(
-        (path.join(__dirname, './pythonServices/environment/Scripts/python')),
-        [(path.join(__dirname, './pythonServices/slice_service.py'))]
-    );
-    childPython.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}.`);
-    });
-    childPython.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}.`);
-    });
-    childPython.on('close', (code) => {
-        console.log(`child process exited with code ${code}.`);
-    });*/
 }); 
  
-
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
