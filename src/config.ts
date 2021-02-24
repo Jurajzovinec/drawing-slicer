be60414b501ecc4eb0d40ea877abcfc83c149693
Juraj Zovinec <juraj.zovinecc@gmail.com>
2021-02-24 10:47:25 +0100

    update: filepaths '\\' replaced by '/'.

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ interface configVariablesInterface {
 
 const config: configVariablesInterface = {
     PYTHON_INTERPRETER_PATH: (process.env.NODE_ENV === 'production') ?
-        '\\usr\\bin\\python' : (path.resolve(process.cwd(), 'python_modules\\Scripts\\python')),
-    PYTHON_SLICE_SERVICE_PATH: (path.resolve(process.cwd(), 'pythonServices\\service_slice_file.py')),
-    PYTHON_INPUT_TEST_SERVICE_PATH: (path.resolve(process.cwd(), 'pythonServices\\service_validate_file.py'))
+        '/usr/bin/python' : (path.resolve(process.cwd(), 'python_modules/Scripts/python')),
+    PYTHON_SLICE_SERVICE_PATH: (path.resolve(process.cwd(), 'pythonServices/service_slice_file.py')),
+    PYTHON_INPUT_TEST_SERVICE_PATH: (path.resolve(process.cwd(), 'pythonServices/service_validate_file.py'))
 }
 
 export default config;
