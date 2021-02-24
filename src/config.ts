24258854d9fdbc394555fb7216cc7dd8f8b31900
Juraj Zovinec <juraj.zovinecc@gmail.com>
2021-02-24 12:32:48 +0100

    Test: '/usr/bin/python' changed to 'python'

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,10 @@ interface configVariablesInterface {
 }
 
 const config: configVariablesInterface = {
+    /*PYTHON_INTERPRETER_PATH: (process.env.NODE_ENV === 'production') ?
+        '/usr/bin/python' : (path.resolve(process.cwd(), 'python_modules/Scripts/python')),*/
     PYTHON_INTERPRETER_PATH: (process.env.NODE_ENV === 'production') ?
-        '/usr/bin/python' : (path.resolve(process.cwd(), 'python_modules/Scripts/python')),
+        'python' : (path.resolve(process.cwd(), 'python_modules/Scripts/python')),
     PYTHON_SLICE_SERVICE_PATH: (path.resolve(process.cwd(), 'pythonServices/service_slice_file.py')),
     PYTHON_INPUT_TEST_SERVICE_PATH: (path.resolve(process.cwd(), 'pythonServices/service_validate_file.py'))
 }
